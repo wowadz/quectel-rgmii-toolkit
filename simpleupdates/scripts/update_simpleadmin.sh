64be7bb14358b1ab11e840e04d63b3aafc63142c
abc <63277113+wowadz@users.noreply.github.com>
2025-02-12 19:32:26 +0800

    Update update_simpleadmin.sh

--- simpleupdates/scripts/update_simpleadmin.sh
+++ simpleupdates/scripts/update_simpleadmin.sh
@@ -60,9 +60,9 @@ REPONAME="quectel-rgmii-toolkit"
 GITTREE="SDXLEMUR"
 GITMAINTREE="SDXLEMUR"
 GITDEVTREE="development-SDXLEMUR"
-GITROOT="https://raw.githubusercontent.com/$GITUSER/$REPONAME/$GITTREE"
-GITROOTMAIN="https://raw.githubusercontent.com/$GITUSER/$REPONAME/$GITMAINTREE"
-GITROOTDEV="https://raw.githubusercontent.com/$GITUSER/$REPONAME/$GITDEVTREE"
+GITROOT="https://gh-proxy.com/raw.githubusercontent.com/$GITUSER/$REPONAME/$GITTREE"
+GITROOTMAIN="https://gh-proxy.com/raw.githubusercontent.com/$GITUSER/$REPONAME/$GITMAINTREE"
+GITROOTDEV="https://gh-proxy.com/raw.githubusercontent.com/$GITUSER/$REPONAME/$GITDEVTREE"
 
 # Define filesystem path
 SIMPLE_ADMIN_DIR="/usrdata/simpleadmin"
